4796fe4d6cfae09ee01606059b55718f19c5c33b
Stephane Wunda <stephanewunda99@gmail.com>
2026-06-16 15:36:27 +0200

    Version 1.00
    Job done , the program works as intended

---
+++ bank.py
@@ -0,0 +1,20 @@
+def main():
+    
+    # get user's greeting
+    
+    greeting = input("Input: ").strip().lower()
+    
+    # Logic detail inside the ReadMe file
+    
+    # Let's work on the logic now
+    
+    if greeting.startswith("hello"):
+        print("Output: $0")
+    elif greeting.startswith("h") and greeting.startswith("h") != "hello":
+        print("Output: $20")
+        
+    else:
+        print("output: $100")
+
+
+main()
